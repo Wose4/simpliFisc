8c5ed948d206d8b2311367a63ccfa13b11c184d6
Wandrille <wandrille@winch4.ai>
2024-12-12 16:42:58 +0100

    new_commit

--- src/components/homepage/HeroSection.tsx
+++ src/components/homepage/HeroSection.tsx
@@ -1,13 +1,17 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const HeroSection: React.FC = () => {
   return (
-    <section className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 to-blue-700 text-white text-center px-4">
-      <h1 className="text-5xl font-bold mb-4">Bienvenue sur notre site</h1>
+    <section className="min-h-screen w-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 to-blue-700 text-white text-center px-4">
+      <h1 className="text-5xl font-bold mb-4">Bienvenue sur SimpliFisc</h1>
       <p className="text-lg mb-6">Découvrez nos services et solutions personnalisées</p>
-      <Button variant="outline" size="lg">
-        En savoir plus
-      </Button>
+      <Link href="/simuler">
+          <Button className="">Simuler</Button>
+      </Link>
+      <Link href="/chat">
+          <Button className="">Chat</Button>
+      </Link>
     </section>
   );
 };
