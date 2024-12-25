9c0fcac3748fd6e7676bc9d36fe6dc02caa3eb3e
Justin <justinleddet@gmail.com>
2024-12-25 14:27:08 +0100

    Feat: Home button for questionnaire

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {!isUserPath && <FullNavBar />}
+        {!isUserPath ? <FullNavBar /> : <MiniNavBar />}
         {children}
       </body>
     </html>
--- src/components/layout/mini_navbar.tsx
+++ src/components/layout/mini_navbar.tsx
@@ -1,38 +1,19 @@
-"use client"
+"use client";
 
 // components/NavBar.jsx
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { useState } from "react";
-
-const profileMenuItems = [
-    { href: "/mon-espace", label: "Mon espace" },
-];
+import { AiFillHome } from "react-icons/ai";
 
 const MiniNavBar = () => {
-
-    return (
-        <nav className="fixed top-4 left-1/2 transform -translate-x-1/2 bg-white/90 backdrop-blur-md shadow-2xl rounded-full px-6 py-3 w-[90vw] flex items-center justify-between">
-            <div className="flex items-center space-x-6">
-                <div className="logo">Logo</div>
-            </div>
-            <div className="flex items-center space-x-6">
-                <div className="border-l h-6 border-gray-300"></div>
-                {profileMenuItems.map(
-                    (item) =>
-                            <Link key={item.href} href={item.href}>
-                                <p className="text-sm font-medium text-black hover:text-black flex items-center transition-colors hover:underline">{item.label}</p>
-                            </Link>
-                )}
-                <Link href="/simuler">
-                    <Button className="">Simuler</Button>
-                </Link>
-                <Link href="/chat">
-                    <Button className="">Chat</Button>
-                </Link>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="fixed top-[5%] left-[93%] z-[9999]">
+      <Link href="/">
+        <button className="flex items-center justify-center h-12 w-12 border-none rounded-xl bg-white/90 shadow-md p-3 hover:bg-gray-50 transition-colors duration-300 ease-in-out">
+          <AiFillHome size={50} className="text-blue-600" />
+        </button>
+      </Link>
+    </nav>
+  );
 };
 
 export default MiniNavBar;
